432ebdb3f54036a1776cb28b9ed87fd2c48f0e86
ohadi <ohadon@gmail.com>
2021-01-10 22:32:45 +0200

    update
    run docker - no --rm option
    new docker scripts, start docker - open exited container
    attache to docker - open new bash on running contatiner

--- runDocker.sh
+++ runDocker.sh
@@ -2,4 +2,5 @@
 
 #docker run -ti --net=host -e DISPLAY -v /tmp/.X11-unixi --rm $1 bash
 # --restart=always 
-docker run --privileged --rm -v /dev/bus/usb:/dev/bus/usb -v /dev:/dev -v /proc:/proc -v /home/ohadi/proj:/home/docker/proj -t -i --net=host -e DISPLAY -v /tmp/.X11-unixi $1 bash
+#docker run --privileged --rm -v /dev/bus/usb:/dev/bus/usb -v /dev:/dev -v /proc:/proc -v /home/ohadi/proj:/home/docker/proj -t -i --net=host -e DISPLAY -v /tmp/.X11-unixi $1 bash
+docker run --privileged -v /dev/bus/usb:/dev/bus/usb -v /home/ohadi/proj:/home/docker/proj -t -i --net=host -e DISPLAY -v /tmp/.X11-unixi $1 bash
