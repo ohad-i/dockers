6cd65bf0f1f535fe009df1f4a3404cd52296d2ad
ohadi <ohadon@gmail.com>
2021-09-16 13:04:10 +0300

    update

--- runOnceDocker.sh
+++ runOnceDocker.sh
@@ -2,5 +2,5 @@
 
 #docker run -ti --net=host -e DISPLAY -v /tmp/.X11-unixi --rm $1 bash
 # --restart=always 
-docker run --privileged -v /dev/bus/usb:/dev/bus/usb -v /dev:/dev -v /proc:/proc -v $HOME/proj:/home/docker/proj -t -i --net=host -e DISPLAY -v /tmp/.X11-unixi $1 bash
+docker run --privileged -v /dev/bus/usb:/dev/bus/usb -v /dev:/dev -v /proc:/proc -v $HOME/proj:/home/docker/proj -t -i --net=host -e DISPLAY -v /tmp/.X11-unixi --rm $1 bash
 #docker run --privileged -v /dev/bus/usb:/dev/bus/usb -v $HOME:/home/docker/ -t -i --net=host -e DISPLAY -v /tmp/.X11-unixi $1 bash
